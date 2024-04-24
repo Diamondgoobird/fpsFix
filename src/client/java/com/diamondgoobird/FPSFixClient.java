f2ce0c46e5c047be213d0e9dfac3859871f649a0
Diamondgoobird <birdsplaymc2@gmail.com>
2024-04-24 16:03:30 -0700

    Added command to set fps (/fps)

--- src/client/java/com/diamondgoobird/FPSFixClient.java
+++ src/client/java/com/diamondgoobird/FPSFixClient.java
@@ -1,10 +1,29 @@
 package com.diamondgoobird;
 
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.minecraft.client.MinecraftClient;
+
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.argument;
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
 
 public class FPSFixClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		ClientCommandRegistrationCallback.EVENT.register(
+				(dispatcher, registryAccess) -> {
+					dispatcher.register(
+							literal("fps").then(
+									argument("fps", IntegerArgumentType.integer()).executes(
+											context -> {
+												MinecraftClient.getInstance().options.getMaxFps().setValue(IntegerArgumentType.getInteger(context, "fps"));
+												return 0;
+											}
+									)
+							)
+					);
+				}
+		);
 	}
 }
